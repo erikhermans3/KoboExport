10ef65fff78f0ecd9a90015d180b7b7346ad8216
Adolf Frederik <Adolf@Adolfs-Mac-mini.local>
2015-04-15 08:31:04 +0200

    Initial Commit

---
+++ KoboExport/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  KoboExport
+//
+//  Created by Adolf Frederik on 4/15/15.
+//  Copyright (c) 2015 Erik Hermans. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
